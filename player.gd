5c6d4dbb30309417856f6e0f8e1a43220c2db471
Robert Wittman <robkwittman@gmail.com>
2024-01-05 10:17:02 -0500

    Add mobs, spawning, and jump functionality

--- player.gd
+++ player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
 @export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
+@export var jump_impulse = 20
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
@@ -13,14 +13,18 @@ func _physics_process(delta):
 
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
+		print("Move right")
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
+		print('Move left')
 		direction.x -= 1
 	if Input.is_action_pressed("move_back"):
+		print("move back")
 		# Notice how we are working with the vector's x and z axes.
 		# In 3D, the XZ plane is the ground plane.
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
+		print("move forward")
 		direction.z -= 1
 
 	if direction != Vector3.ZERO:
@@ -33,6 +37,30 @@ func _physics_process(delta):
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
+
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+		
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+	
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		# If the collision is with ground
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			# we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# If so, we squash it and bounce.
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				# Prevent further duplicate calls.
+				break
